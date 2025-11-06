ae2b87689803aa20bbaf9e90b2af1a185173df88
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-11-06 01:29:18 +0000

    create program class with basic render

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,50 @@
-const container: HTMLElement | null = document.getElementById("root");
+import * as THREE from "three";
 
-if (container === null) {
-    throw new Error("Root element not found.");
+class Program {
+    private container: HTMLElement;
+
+    constructor() {
+        const container: HTMLElement | null = document.getElementById("root")!;
+        this.container = container
+    }
+
+    public run(): void {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        const color = 0xFFFFFF;
+        const intensity = 3;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+
+        const map: HTMLCanvasElement | null = this.container.querySelector("#map")!;
+
+        const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
+            antialias: true, canvas: map
+        });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setAnimationLoop(animate);
+        document.body.appendChild(renderer.domElement);
+
+        function animate() {
+
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+
+        }
+    }
 }
 
-const welcome: HTMLElement = document.createElement("div");
-welcome.textContent = "Caern.";
-welcome.style.fontSize = "24px";
+const program = new Program();
+program.run();
 
-container.appendChild(welcome);
