cbea4ca20cb3adda56e30c73fc628a059644b400
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-11-07 21:46:43 +0000

    remove tutorial cube

--- src/SceneManager.ts
+++ src/SceneManager.ts
@@ -8,16 +8,12 @@ export class SceneManager {
     constructor(canvas: HTMLCanvasElement) {
         this._scene = new THREE.Scene();
         this._camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this._camera.position.set(0, 0, 20);
         this._renderer = new THREE.WebGLRenderer({ canvas })
     }
 
     public render(): void {
-        const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-        const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-        const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
-        this._scene.add(cube);
 
-        this._camera.position.z = 5;
 
         const color: number = 0xFFFFFF;
         const intensity: number = 3;
@@ -26,14 +22,8 @@ export class SceneManager {
         this._scene.add(light);
 
         this._renderer.setSize(window.innerWidth, window.innerHeight);
-        this._renderer.setAnimationLoop(() => this._animate(animateCube));
+        this._renderer.setAnimationLoop(() => this._animate());
         document.body.appendChild(this._renderer.domElement);
-
-        function animateCube() {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-        }
-
     }
 
     private _animate(...params: Array<() => void>): void {
