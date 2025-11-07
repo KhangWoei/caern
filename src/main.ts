5b788e3e9243ca74aa46f2a0be22be1d8a5adcf8
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-11-07 17:29:03 +0000

    wrap scene mangaement to its own type

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,64 @@
 import * as THREE from "three";
 
 class Program {
-    private container: HTMLElement;
+    private _container: HTMLElement;
+    private _sceneManager: SceneManager;
 
     constructor() {
         const container: HTMLElement | null = document.getElementById("root")!;
-        this.container = container
+        this._container = container;
+
+        const map: HTMLCanvasElement | null = this._container.querySelector("#map")!;
+        this._sceneManager = new SceneManager(map);
     }
 
     public run(): void {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this._sceneManager.render()
+    }
+}
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+class SceneManager {
+    private readonly _renderer: THREE.WebGLRenderer;
+    private readonly _scene: THREE.Scene;
+    private readonly _camera: THREE.Camera;
 
-        camera.position.z = 5;
+    constructor(canvas: HTMLCanvasElement) {
+        this._scene = new THREE.Scene();
+        this._camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this._renderer = new THREE.WebGLRenderer({ canvas })
+    }
 
-        const color = 0xFFFFFF;
-        const intensity = 3;
-        const light = new THREE.DirectionalLight(color, intensity);
+    public render(): void {
+        const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+        const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+        const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+        this._scene.add(cube);
+
+        this._camera.position.z = 5;
+
+        const color: number = 0xFFFFFF;
+        const intensity: number = 3;
+        const light: THREE.Light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
-        scene.add(light);
+        this._scene.add(light);
 
-        const map: HTMLCanvasElement | null = this.container.querySelector("#map")!;
+        this._renderer.setSize(window.innerWidth, window.innerHeight);
+        this._renderer.setAnimationLoop(() => this._animate(animateCube));
+        document.body.appendChild(this._renderer.domElement);
 
-        const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
-            antialias: true, canvas: map
-        });
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setAnimationLoop(animate);
-        document.body.appendChild(renderer.domElement);
-
-        function animate() {
-
+        function animateCube() {
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
-
-            renderer.render(scene, camera);
-
         }
+
+    }
+
+    private _animate(...params: Array<() => void>): void {
+        params.forEach(animation => {
+            animation()
+        });
+
+        this._renderer.render(this._scene, this._camera);
     }
 }
 
